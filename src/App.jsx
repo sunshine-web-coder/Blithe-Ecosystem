8c752a71eb84f398de4d53590829ade2dca42272
webdev0202 <webdev0202@yandex.com>
2022-10-15 09:38:40 +0100

    latest update

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import RequestForm from './pages/RequestForm';
 import TermOfService from './pages/TermOfService';
+import Disclaimer from './pages/Disclaimer';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/form" element={<RequestForm />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms-of-service" element={<TermOfService />} />
+        <Route path="/disclaimer" element={<Disclaimer />} />
       </Routes>
     </BrowserRouter>
   )
