a159c6a89df5c7b0b840b125b373842dfbdca752
webdev0202 <webdev0202@yandex.com>
2022-10-17 12:38:15 +0100

    new commit

--- src/pages/PrivacyPolicy/index.jsx
+++ src/pages/PrivacyPolicy/index.jsx
@@ -9,11 +9,11 @@ const PrivacyPolicy = () => {
         <div className="banner">
           <img src="https://iili.io/Zhlb4a.md.jpg" alt="pBanner" />
         </div>
-        <div className="title">Blithe Wallet: Privacy Policy</div>
+        <div className="title">Blithe Wallet Privacy Policy</div>
         <div className="subTitle">
-          This Privacy Notice illustrates how Blithe Wallet (also denoted as
-          “us, we, our”) collects, uses, discloses, and protects the personal
-          information/data of users of our platform on{" "}
+          This Privacy Policy explains how Blithe Wallet (“us, we, our”)
+          collects, uses, discloses, and protects the Personal Information/Data
+          of users of our platform on{" "}
           <a target="_blank" href="https://blithewallet.com/">
             blithewallet.com
           </a>
@@ -23,41 +23,33 @@ const PrivacyPolicy = () => {
         </div>
         <div className="pPContent">
           <div>
-            <div className="title">GENERAL NOTE ON OUR PRIVACY POLICY</div>
+            <div className="title">OVERVIEW OF PRIVACY NOTICE</div>
             <div className="contt">
               <p>
-                Your data is essential to Blithe Wallet and helps us serve you
-                better. Asides from being important to us, it is vital to help
-                us contain legal discrepancies and ensure you comply with our
-                terms of use/service all the time.{" "}
-              </p>
-              <p>
-                We do not share or sell your personal data to third parties. We
-                are only bent on providing you with our services to the best of
-                our abilities. We also implore you to comply with our Privacy
-                Policy to help us serve you better.{" "}
+                Blithe Wallet uses your data to serve you better. As applicable
+                by law, we use your data in legal situations based on what
+                you’ve provided to us. We do not share or sell your personal
+                data to third parties. We are only bent on providing you with
+                our services to the best of our abilities when we request your
+                Personal Information. We also implore you to comply with our
+                Privacy Policy to help us serve you better.
               </p>
               <p>
                 By using our Product or Service, through its mobile or web
                 application, or visiting and accessing directly via our official
                 website, you agree to our Privacy Policy. You agree that we can
                 collect your personal data/information and use it in compliance
-                with the law, to enhance your experiences on our Platform.{" "}
+                with the law, to enhance your experiences on our Platform.
               </p>
               <p>
                 We only collect the necessary information from you. If by any
-                means, you provide additional information to us, we’ll treat it
-                as a voluntary effort from your end and at your discretion.{" "}
+                means, you provide additional information not requested by us,
+                we will treat it as though you have willingly done so.
               </p>
               <p>
                 This Privacy Notice complies with the latest developments
                 effectuated by the General Data Protection Regulation (GDPR) and
-                covers those residing in the European Economic Area (EEA).
-              </p>
-              <p>
-                We reserve the right to update this Privacy Notice whenever we
-                choose. We’ll always notify our users of any update on our
-                Privacy Policy electronically via push notifications or mail.
+                covers those residing in the European Union (EU).
               </p>
             </div>
           </div>
@@ -68,34 +60,40 @@ const PrivacyPolicy = () => {
             </div>
             <div className="contt">
               <p>
-                We are a non-custodial wallet and also an ecosystem of various
-                DeFi services. So the way we collect your information, and what
-                information we collect, may vary based on your intent on our
-                platform. For more information on what we offer, you may see our
+                We are a non-custodial wallet, and the information we collect
+                from you is limited. Nevertheless, you may find additional DeFi
+                features and products within our platform which require you to
+                verify your identity before using the service. The information
+                we collect from you depends on the service you use on Blithe
+                Wallet. Our services and how you may use them are properly
+                explained in our{" "}
                 <a href="/terms-of-service"> Terms of Service/Use</a>.
               </p>
               <p>
-                We basically collect your basic profile information and contact
-                info, including phone numbers and/or email addresses which may
-                be gotten whenever you file a complaint or send us a request,
-                suggestion, or report a bug.
+                We basically collect your basic profile information (full name,
+                username, and password) and contact info (phone numbers and/or
+                email addresses). We may collect this data when you file a
+                complaint or send us a request, suggestion, or report a bug.
               </p>
-              <p>Categorically, the information we collect is as follows:</p>
+              <p>
+                The information we collect is categorically explained as
+                follows:
+              </p>
               <ul>
                 <li>
-                  Registration Information: Information we get when you sign up
-                  for any of our products or services requiring you to enter
-                  your basic information.
+                  Registration Information: We may collect your basic profile
+                  information when signing up for a third-party product or
+                  service on Blithe Wallet.
                 </li>
                 <li>
                   Customer support information: We may collect your information
                   when you file a complaint to our customer support center. This
-                  may include your email address and phone number.{" "}
+                  may include your email address and phone number.
                 </li>
                 <li>
                   Product/Service Usage Information: We may collect data on your
-                  operating system, IP address, device and browser used to
-                  access our platform, data and time you accessed our platform
+                  operating system, IP address, device, and browser used to
+                  access our platform, data, and time you accessed our platform
                   or service, among others.
                 </li>
               </ul>
@@ -107,19 +105,19 @@ const PrivacyPolicy = () => {
             <div className="contt">
               <p>
                 We collect these data to communicate with our users effectively
-                and quickly combat system and application complications from our
-                backend. Collecting your personal information also helps us to
-                keep records and investigate user reports legally and
-                technically. In advanced cases that involve the authorities,
-                your personal information may be required.
+                and quickly to combat technical issues from our backend.
+                Collecting your personal information also helps us to keep
+                records and investigate technical user reports. In advanced
+                cases that involve the authorities or law, your personal
+                information may be required to sort things out.
               </p>
               <p>
-                We can only notice or monitor certain areas of your interaction
-                with our platform when you consent to our request for your
-                personal information. You can choose to cancel or withdraw your
-                consent to our usage of your personal information and we’ll
-                outrightly stop using your data for any reason; however, this
-                will be accompanied by the cancellation of our service usage.
+                We can only track certain areas of your interaction with our
+                platform when you consent to our request for your personal
+                information. You can choose to cancel or withdraw your consent
+                to our usage of your personal information and we’ll outrightly
+                stop using your data for any reason, even when you stop using
+                our services.
               </p>
             </div>
           </div>
@@ -134,8 +132,7 @@ const PrivacyPolicy = () => {
                 on our platform. During verification, we could collect and
                 process your data to secure a communication link with you and
                 serve you better. You reserve the full right to terminate our
-                use of your data by sending us a request electronically via mail{" "}
-                <br />
+                use of your data by sending us a request electronically via mail
                 (at{" "}
                 <a href="mailto:support@blithewallet.com">
                   support@blithewallet.com
@@ -153,7 +150,7 @@ const PrivacyPolicy = () => {
                         information even after deleting or uninstalling your
                         wallet account or address(es). Your data may remain in
                         our database for as long as we choose to keep it there.
-                        This is to help observe regularity on our platform, and
+                        This is to help maintain regularity on our platform, and
                         uncommon cases of legal actions.
                       </p>
                     </div>
@@ -166,24 +163,47 @@ const PrivacyPolicy = () => {
                       <p>
                         We do our best to protect the data of our users
                         according to the law guiding data collection and usage
-                        rights. However, we do not control how third parties
-                        integrated on our platform use your data. You may need
-                        to read their privacy policies as well before using a
-                        third-party service on our platform to ensure we all are
-                        on the same page.
+                        rights (including in the European Union (EU)). However,
+                        we do not control how our third parties or affiliates
+                        may use your Personal Information. You must read
+                        third-party privacy policies before using their Services
+                        on our platform to ensure we all are on the same page.
                       </p>
                       <p>
                         We use standard security encryption and decryption
-                        technologies to safeguard your data, ensuring no third
-                        party can view or see the information you give us at
-                        all. This implies a peer-to-peer
-                        connection/communication between you and us.{" "}
+                        technologies to safeguard your data, making sure that no
+                        third party can see or intercept the information you
+                        give us. This implies a peer-to-peer
+                        connection/communication between you and us.
                       </p>
                       <p>
-                        We reserve the right to sell your data to our third
-                        parties such as agents, affiliates, representatives,
-                        etc. This is to improve their services on our platform.
-                        We also may not control how they will use your data.
+                        We reserve the right to sell your data to our select
+                        third-party agents, affiliates, representatives, etc—who
+                        use or represent our Product or Service. We may not
+                        control how these affiliates or agents use your data.
+                      </p>
+                    </div>
+                  </div>
+                </li>
+                <li className="LJhgvn">
+                  <div className="list-Hmn">
+                    <div className="title">CHILD USE POLICY</div>
+                    <div className="contt">
+                      <p>
+                        Before using our Product or Service, you must be at
+                        least eighteen (18) years old. We do not support usage
+                        for individuals under the age of eighteen (18). We will
+                        not willingly collect information from minors below
+                        thirteen (13). However, you may use Blithe Wallet on
+                        behalf of a minor: any information you provide on Blithe
+                        Wallet will be treated as yours. We implore you to shoot
+                        us an email at{" "}
+                        <a href="mailto:support@blithewallet.com">
+                          support@blithewallet.com
+                        </a>{" "}
+                        to learn more about how Child Use Policy of Blithe
+                        Wallet, or if you suspect our Product or Service is
+                        being used by a minor around you.
                       </p>
                     </div>
                   </div>
@@ -193,12 +213,49 @@ const PrivacyPolicy = () => {
           </div>
 
           <div>
+            <div className="title">CONDITIONS TO USE OUR PRIVACY NOTICE</div>
+            <div className="contt">
+              <p>
+                By using our Product and Service, you agree to do so, as
+                applicable by law, in compliance with our privacy policy. Any
+                complaint or dispute over using Blithe Wallet must be subject to
+                our Privacy Policy and Terms of Service. If you’re not clear
+                about our Privacy Policy, feel free to contact us, and we’ll
+                clear things up for you.
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <div className="title">UPDATES TO PRIVACY POLICY</div>
+            <div className="contt">
+              <p>
+                We reserve the right to update this Privacy Notice whenever we
+                choose. We’ll always notify our users of any update on our
+                Privacy Policy electronically via push notifications or mail. We
+                revise our Privacy Notice to ensure it complies with the latest
+                data use policies worldwide and updates to our Services and
+                Products.
+              </p>
+              <p>
+                We advise that you occasionally review this Privacy Notice to
+                make sure we are all always on the same page. We will notify you
+                of any changes to our Privacy Notice, at the time of the
+                changes.
+              </p>
+            </div>
+          </div>
+
+          <div>
             <div className="title">CONTACT AND SUPPORT</div>
             <div className="contt">
               <p>
                 If you have any complaints regarding our privacy policy;
-                suggestions or reports concerning a violation of your personal
-                information, you can reach us via mail: <a href="mailto:support@blithewallet.com">support@blithewallet.com</a>
+                suggestions or reports concerning a violation of your Personal
+                Information or our Terms of Service, you can reach us via mail:
+                <a href="mailto:support@blithewallet.com">
+                  support@blithewallet.com
+                </a>.
               </p>
             </div>
           </div>
